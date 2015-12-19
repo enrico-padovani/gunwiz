b0434c60e9b72a60bbee08d754828aa4214c1da4
enrico-padovani <enrico.padovani@alchemists.it>
2015-12-19 22:40:25 +0100

    Full navigation cycle sample with simple validation

--- demo/app.js
+++ demo/app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var path = require('path');
 
 var app = express();
@@ -9,8 +10,10 @@ app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 // redefine express's default settings for locating views
 app.set('views', __dirname + '/views');
-
 app.set('view engine', 'vash');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true}));
 
 // define routes
 app.get('/', function (req, res) {
@@ -18,7 +21,19 @@ app.get('/', function (req, res) {
 });
 
 app.get('/step1', function (req, res) {
-    res.render('step1', { title: 'Step 1', errorMessage: ''});
+    res.render('demo-step1', { title: 'Demo 1/2', errorMessage: ''});
+});
+
+app.post('/step1', function (req, res) {
+    if (req.body.requiredField.length < 3){
+        res.render('demo-step1', { title: 'Demo 1/2', errorMessage: 'requiredField must be >= 3 chars'});
+        return;
+    }
+    res.render('demo-step2', { title: 'Demo 2/2', errorMessage: ''});
+});
+
+app.post('/step2', function (req, res) {
+    res.render('index', { title: 'Menu', errorMessage: 'The last answer was ' + req.body.answer});
 });
 
 app.listen(process.env.PORT || 3000);
---
+++ demo/scripts/controllers/demo.controller.js
@@ -0,0 +1,28 @@
+﻿(function(window, document) {
+    'use strict';
+
+    function demoController() {
+
+        function setAnswer(value) {
+            document.getElementById('answer').value = value;
+        }
+
+        function noCommand() {
+            setAnswer('false');
+            gunwiz.functions.submit();
+        }
+
+        function yesCommand() {
+            setAnswer('true');
+            gunwiz.functions.submit();
+        }
+
+        return {
+            noCommand: noCommand,
+            yesCommand: yesCommand
+        }
+    }
+
+    gunwiz.controller('demoController', demoController);
+
+})(window, document);
